ca12878bbc570846094a082d0e492db00f4cbf40
Morgan Ling <morganhz.ling@gmail.com>
2018-02-03 18:53:05 -0800

    npm express and request

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-a
+var express = require('express');
+var app = express();
+
+var request = require('request');
+
+
+
+
+
+
+
+
+
+
+
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
+
+if (!module.parent) {
+  app.listen(3000);
+  console.log('Listening on port 3000');
+}
